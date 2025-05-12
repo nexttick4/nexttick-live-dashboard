45bb658c1ca01c857f15142ec88b2373ad1fcec1
nexttick4 <stadesasha@gmail.com>
2025-05-12 13:40:34 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
